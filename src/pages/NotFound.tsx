8feaaf220aa584b7dbcd98a645cf1b62f7c1b1a0
Isai Jesus <isaijesus02@gmail.com>
2022-02-11 23:54:40 -0600

    Styles NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
+import "../App.css";
+
 const NotFound = () => {
   return (
-    <div>
-      <h1>Not found 404</h1>
-    </div>
+    <main className="container">
+      <h1 className="title-404">Ooops!</h1>
+      <h2 className="subtitle-404">Page not found</h2>
+      <img className="img-404" src="/images/notFound.png" alt="404"/>
+      <p className="instruction-home">This page doesn't exist or was removed. We suggest you back home page.</p>
+      <Link to="/" className="btn">Go Home Page</Link>
+    </main>
   );
 };
 
