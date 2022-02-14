796e456d67b2b7dc99622eca2ac443803cae5aca
Isai Jesus <isaijesus02@gmail.com>
2022-02-13 18:22:05 -0600

    Time to work and rest

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <main className="container">
       <h1 className="title-home">Control your work and rest times with the pomodoro technique.</h1>
-      <h3 className="instruction-home">Choose your work times and shorter break times to be more efficient.</h3>
+      <h3 className="instruction-home">Choose your work times and rest times to be more efficient.</h3>
       <img className="img-home" src="/images/clock.png" alt="Pomodoro Clock"/>
       <Link className="btn" to="/settings">Start</Link>
     </main>
